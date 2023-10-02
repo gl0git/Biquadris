35f7153e8cb5e43351611b62560f25eff1f0f7cf
gl0git <williamcolinsgeorge@gmail.com>
2023-10-02 14:33:22 -0400

    Information about code on request

---
+++ project/code_available_on_request.cc
@@ -0,0 +1,8 @@
+// This was a project for a University of Waterloo object-oriented programming course, CS246.
+// 
+// The rules of my university do not allow me to share code publicly on the internet as it would compromise
+// their academic integrity rule, Policy 71.
+//
+// For this reason, I am providing the code for the project on request. Just shoot me a message and I'll send it over.
+//
+// You can also get information about the project via the README.
